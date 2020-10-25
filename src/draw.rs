04af5c7c9a9c36b827261f4bf5dca0992a25380e
Jori <jvalkenhoff@outlook.com>
2020-10-25 22:00:59 +0100

    first commit

---
+++ src/draw.rs
@@ -0,0 +1,55 @@
+// importing crates
+use piston_window::{rectangle, Context, G2d};
+use piston_window::types::Color;
+
+// blocks will scale up 25 pixels
+const BLOCK_SIZE: f64 = 25.0;
+
+// takes a coordinate and scales it up based on BLOCK_SIZE
+pub fn to_coord(game_coord: i32) -> f64 {
+    (game_coord as f64) * BLOCK_SIZE
+}
+
+pub fn to_coord_u32(game_coord: i32) -> u32 {
+    to_coord(game_coord) as u32
+}
+
+
+// draws a block: takes a color, x and y coordinates, context and 2D graphics buffer
+pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
+    let gui_x = to_coord(x);
+    let gui_y = to_coord(y);
+
+    rectangle(
+        color,
+        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+        con.transform,
+        g,
+    );
+}
+
+// draws rectangles, almost the same as draw_block
+pub fn draw_rectangle(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32, 
+    con: &Context,
+    g: &mut G2d,
+) {
+    let x = to_coord(x);
+    let y = to_coord(y);
+
+    rectangle(
+        color,
+        [
+            x, 
+            y, 
+            BLOCK_SIZE * (width as f64), 
+            BLOCK_SIZE * (height as f64),
+        ],
+        con.transform,
+        g,
+    );
+}
